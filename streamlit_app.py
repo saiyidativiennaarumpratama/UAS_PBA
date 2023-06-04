37457d620f280857aaa4eb71134fe071a1ebe4dd
saiyidativiennaarumpratama <84006659+saiyidativiennaarumpratama@users.noreply.github.com>
2023-06-04 16:38:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,13 @@ from PIL import Image
 image = Image.open('assets/logo-myxl.jpg')
 st.image(image, use_column_width=True)
 st.title("ANALISIS SENTIMEN ULASAN APLIKASI My XL")
-st.write("Author: Kharisma Intan Safitri, Vania, Rima")
-st.write("Natural Language Processing Project for Final Exam")
+st.write("""
+    ##### Kelompok 3:\n
+     Kharisma Intan Safitri 200411100010 \n
+     Saiyidati Vienna Arum P 200411100018 \n
+     Siti Ulun Nuha Karimah 200411100127 \n
+     """)
+st.write("### Natural Language Processing Project for Final Exam")
 
 # load model
 model = joblib.load('model/modelSVM.pkl')
